6b03be4facb3c93f469e380f47f2965ff4e31ae5
Shaofeng <marley92@163.com>
2018-01-07 11:42:13 +0800

    Initial Commit

---
+++ LCMarqueeView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LCMarqueeView
+//
+//  Created by Lockeme on 2018/1/7.
+//  Copyright © 2018年 Lockeme. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LCMarqueeView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LCMarqueeView
+//
+//  Created by Lockeme on 2018/1/7.
+//  Copyright © 2018年 Lockeme. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
